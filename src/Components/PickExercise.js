801db5fce20fee5991cbeb391119a5f36683b205
Bryan <irwinb14@gmail.com>
2023-03-21 12:50:50 -0700

    single exercise remove

--- src/Components/PickExercise.js
+++ src/Components/PickExercise.js
@@ -1,18 +1,87 @@
+// import React from "react";
+// import { useState } from "react";
+// import GenerateWorkout from "./GenerateWorkout";
+
+// const PickExercise = () => {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     if (count < 3) setCount((count) => (count += 1));
+//     console.log(count);
+//   }
+
+//   function decrement() {
+//     if (count > 0) setCount((count) => (count -= 1));
+//     console.log(count);
+//   }
+
+//   return (
+//     <div>
+//       <ul>
+//         <li>
+//           <GenerateWorkout />
+//         </li>
+//         {count > 0 ? (
+//           <div>
+//             <li>
+//               <GenerateWorkout />
+//               <button onClick={decrement}>x</button>
+//             </li>
+//           </div>
+//         ) : null}
+//         {count > 1 ? (
+//           <li>
+//             <GenerateWorkout />
+//             <button onClick={decrement}>x</button>
+//           </li>
+//         ) : null}
+//         {count > 2 ? (
+//           <li>
+//             <GenerateWorkout />
+//             <button onClick={decrement}>x</button>
+//           </li>
+//         ) : null}
+//       </ul>
+//       <button onClick={increment}>Add a muscle group</button>
+//     </div>
+//   );
+// };
+
+// export default PickExercise;
+
 import React from "react";
 import { useState } from "react";
 import GenerateWorkout from "./GenerateWorkout";
 
 const PickExercise = () => {
+  const [listItems, setListItems] = useState([]);
   const [count, setCount] = useState(0);
 
-  function increment() {
-    if (count < 3) setCount((count) => (count += 1));
-    console.log(count);
+  function addItem() {
+    if (count < 3) {
+      setCount((count) => (count += 1));
+      setListItems([
+        ...listItems,
+        <ListItem key={listItems.length} index={listItems.length} />,
+      ]);
+    }
   }
 
-  function decrement() {
-    if (count > 0) setCount((count) => (count -= 1));
-    console.log(count);
+  function ListItem() {
+    const [removed, setRemoved] = useState(false);
+    return removed ? null : (
+      <li>
+        <GenerateWorkout />
+        <button
+          onClick={() => {
+            setCount((count) => (count -= 1));
+            setRemoved((removed) => !removed);
+          }}
+        >
+          x
+        </button>
+      </li>
+    );
   }
 
   return (
@@ -21,28 +90,11 @@ const PickExercise = () => {
         <li>
           <GenerateWorkout />
         </li>
-        {count > 0 ? (
-          <div>
-            <li>
-              <GenerateWorkout />
-              <button onClick={decrement}>x</button>
-            </li>
-          </div>
-        ) : null}
-        {count > 1 ? (
-          <li>
-            <GenerateWorkout />
-            <button onClick={decrement}>x</button>
-          </li>
-        ) : null}
-        {count > 2 ? (
-          <li>
-            <GenerateWorkout />
-            <button onClick={decrement}>x</button>
-          </li>
-        ) : null}
+        {listItems.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
       </ul>
-      <button onClick={increment}>Add a muscle group</button>
+      <button onClick={addItem}>Add a muscle group</button>
     </div>
   );
 };
