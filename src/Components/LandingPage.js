80d4c1dd00856cd1b51715913b6d01e1e1a103cf
Bryan <irwinb14@gmail.com>
2023-03-22 12:48:50 -0700

    intensity/experience

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -2,24 +2,25 @@ import React from "react";
 import { FaRunning, FaSwimmer } from "react-icons/fa";
 import { IoIosFitness } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
+import logo from "../logo/FitBot.png";
 
 export default function LandingPage() {
   const navigate = useNavigate();
   return (
     <div className="landingContainer">
-      <h1 className="landingTitle">Welcome to FitBot</h1>
-      <div className="landingIconsContainer">
-        <div className="landingIcons">
-          <FaRunning color="#A7FF37" size="96" />
-          <IoIosFitness color="#A7FF37" size="96" />
-          <FaSwimmer color="#A7FF37" size="96" />
-        </div>
-      </div>
+      <img
+        className="landingPageLogo"
+        src={logo}
+        alt="FitBot"
+        width={200}
+        height={300}
+      />
+      {/* <h1 className="landingTitle">Welcome to FitBot</h1> */}
       <div className="landingButtonsContainer">
         <button
           className="landingButton"
           onClick={() => {
-            navigate('/pickExercise');
+            navigate("/pickExercise");
           }}
         >
           Continue as Guest
@@ -32,6 +33,13 @@ export default function LandingPage() {
         >
           Login or Sign Up
         </button>
+      </div>
+      <div className="landingIconsContainer">
+        <div className="landingIcons">
+          <FaRunning color="#A7FF37" size="96" />
+          <IoIosFitness color="#A7FF37" size="96" />
+          <FaSwimmer color="#A7FF37" size="96" />
+        </div>
       </div>
     </div>
   );
